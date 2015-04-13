f1b4b51f0d1e3ebb1cae3d3156c1200330fbf439
Jonathan Weisberg <jonathan.weisberg@utoronto.ca>
2015-04-13 08:32:27 -0400

    Use Mailcatcher gem in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,17 +37,21 @@ Ergonaut::Application.configure do
 
   # Mailer settings
   config.action_mailer.default_url_options = { host: "localhost:3000" }
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['GMAIL_USER'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'gmail.com',
+  #   user_name:            ENV['GMAIL_USER'],
+  #   password:             ENV['GMAIL_PASSWORD'],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true  }
+  
+  # Mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
     
 end
