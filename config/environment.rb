609863d208b0444505f0375ca4ce620649b52aee
Jonathan Weisberg <jonathan.weisberg@utoronto.ca>
2016-02-17 10:59:23 -0500

    Update some gems for compatibility with new version of mysql; add a corresponding monkey patch.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Ergonaut::Application.initialize!
+
+# Monkey patch for MySQL 5.7
+require File.expand_path('../../lib/patches/abstract_mysql_adapter', __FILE__)
